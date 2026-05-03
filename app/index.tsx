c8df39f72760813aad0786bb9ab7706c3e050924
Isabella <isa021andrade@gmail.com>
2026-05-02 21:04:51 -0300

    chore: adjust content paths for app directory structure (nativewind)

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import { Text, View } from "react-native";
+import "./global.css";
 
 export default function Index() {
   return (
     <View
       className="flex-1 justify-center items-center"
     >
-      <Text className="text-6xl">Edit appjkkllljk/index.tsx to edit this screen.</Text>
+      <Text className="text-6xl">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
