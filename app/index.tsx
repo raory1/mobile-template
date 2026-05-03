c423d092f0ab7a7a2294a7d6bfcc042aab270bad
Isabella <isa021andrade@gmail.com>
2026-05-03 17:33:41 -0300

    add default initial screen

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,10 @@ import "./global.css";
 export default function Index() {
   return (
     <View
-      className="flex-1 justify-center items-center"
-    >
-      <Text className="text-6xl">Edit app/index.tsx to edit this screen.</Text>
+      className="flex-1 justify-center items-center bg-background"
+    > 
+      <Text className="text-6xl text-primary">Welcome!</Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#7A4CF0',
+        'secondary': '#97d378',
+        'accent': '#39affe',
+        'sucess': '#6BBF59',
+        'error': '#E5484D',
+        'background': '#FFFAFC',
+        'text': '#140006',
+      },
+    },
   },
   plugins: [],
 }
